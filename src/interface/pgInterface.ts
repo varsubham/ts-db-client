9f1a8158bccf9b78c7b77e2ac454f54a1bd6681e
varsubham <var.subham@gmail.com>
2022-04-06 23:45:10 +0530

    remove rowMapper interface

--- src/interface/pgInterface.ts
+++ src/interface/pgInterface.ts
@@ -2,7 +2,7 @@ import { Pair } from '../utils/utils';
 import { RowMapper } from '../utils';
 
 export interface IDBClient {
-    fetchAll<R>(query: string, rowMapper: RowMapper<R>): Promise<R[]>
+    fetchAll<R>(query: string, obj: R): Promise<R[]>
     fetchAllUsingField<R>(query: string, field: string): Promise<R[]>
     fetchAllUsingTwoFields<R, T>(query: string, field1: string, field2: string): Promise<Pair<R, T>[]>
 }
